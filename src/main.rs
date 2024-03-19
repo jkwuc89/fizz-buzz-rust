6b647c677f5b99991e4b668cc0adee7c9c20c683
Keith Wedinger <keith@emporatitle.com>
2024-03-19 10:02:12 -0400

    Very simple implementation using if/then/else

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    for number in 1..=100 {
+        fizz_buzz_using_if_then_else(number)
+    }
+}
+
+fn fizz_buzz_using_if_then_else(number: i32) {
+    if number % 15 == 0 {
+        println!("fizzbuzz");
+    } else if number % 3 == 0 {
+        println!("fizz");
+    } else if number % 5 == 0 {
+        println!("buzz");
+    } else {
+        println!("{}", number);
+    }
 }
